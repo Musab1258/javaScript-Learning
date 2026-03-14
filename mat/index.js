0d889d35a917caabb9f4639e72b29276bf9f35db
Musab1258 <habeebmusab@gmail.com>
2026-03-14 22:11:38 +0100

    done

---
+++ mat/index.js
@@ -0,0 +1,17 @@
+let synonyms = ["plethora", "orange", "cornocopia"];
+let yourName = "Aishah";
+
+console.log(synonyms);
+console.log(yourName);
+
+console.log(1 + 1);
+console.log(2 * 3);
+console.log(10 / 2);
+console.log(5 - 2);
+
+synonyms.push("abundance");
+console.log(synonyms);
+
+let yourAge = 21;
+
+console.log(yourAge);
